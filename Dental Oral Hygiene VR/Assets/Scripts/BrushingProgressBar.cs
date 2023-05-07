f4a23cb73df9097db67624f08e8dd6b736ff6e0d
CheongYeeMing <e0526001@u.nus.edu>
2023-05-07 23:32:12 +0800

    Update Script for Brushing Progress Bar

--- Dental Oral Hygiene VR/Assets/Scripts/BrushingProgressBar.cs
+++ Dental Oral Hygiene VR/Assets/Scripts/BrushingProgressBar.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class BrushingProgressBar : MonoBehaviour
 {
+    private float value;
+    [SerializeField] private int stages;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        value = 100/stages;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void IncrementProgress() {
+        // Increase by some set amount
+    }
+
+    private void UpdateProgressBar() {
+        // Update Progress Bar UI
+    }
+
+    private void CompleteBrushing() {
+        // Progress to Scene 3b
     }
 }
