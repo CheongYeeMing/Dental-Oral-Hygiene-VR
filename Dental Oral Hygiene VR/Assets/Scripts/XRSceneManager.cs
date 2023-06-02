f789bd37062c6279b8195067cc6a928bfffa6b6f
CheongYeeMing <e0526001@u.nus.edu>
2023-06-02 14:50:25 +0800

    Add XR Origin Rig Animation intro_scene_2

---
+++ Dental Oral Hygiene VR/Assets/Scripts/XRSceneManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRSceneManager : MonoBehaviour
+{
+    // Animation Scenes for Upper and Lower Teeth 
+    protected const string INTRO_SCENE_2 = "intro_scene_2";
+    protected const string UPPER_TEETH_A = "upper_teeth_a";
+    protected const string UPPER_TEETH_B = "upper_teeth_b";
+    protected const string UPPER_TEETH_C = "upper_teeth_c";
+    protected const string UPPER_TEETH_D = "upper_teeth_d";
+    protected const string UPPER_TEETH_E = "upper_teeth_e";
+    protected const string UPPER_TEETH_F = "upper_teeth_f";
+    protected const string LOWER_TEETH_A = "lower_teeth_a";
+    protected const string LOWER_TEETH_B = "lower_teeth_b";
+    protected const string LOWER_TEETH_C = "lower_teeth_c";
+    protected const string LOWER_TEETH_D = "lower_teeth_d";
+    protected const string LOWER_TEETH_E = "lower_teeth_e";
+    protected const string LOWER_TEETH_F = "lower_teeth_f";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
