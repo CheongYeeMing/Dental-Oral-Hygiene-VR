2bf2bc33cbd0e7ee77358ae865c244eea60d160a
CheongYeeMing <e0526001@u.nus.edu>
2023-06-07 19:57:30 +0800

    Scene 3 Interdenal Brushing

--- Dental Oral Hygiene VR/Assets/Scripts/Monologue.cs
+++ Dental Oral Hygiene VR/Assets/Scripts/Monologue.cs
@@ -8,11 +8,28 @@ public class Monologue : MonoBehaviour
     public List<Sequence> Sequences;
     public int sequenceNumber;
 
+    public List<GameObject> UPPER_TEETH_A;
+    public List<GameObject> UPPER_TEETH_B;
+    public List<GameObject> UPPER_TEETH_C;
+    public List<GameObject> UPPER_TEETH_D;
+    public List<GameObject> UPPER_TEETH_E;
+    public List<GameObject> UPPER_TEETH_F;
+    public List<GameObject> LOWER_TEETH_A;
+    public List<GameObject> LOWER_TEETH_B;
+    public List<GameObject> LOWER_TEETH_C;
+    public List<GameObject> LOWER_TEETH_D;
+    public List<GameObject> LOWER_TEETH_E;
+    public List<GameObject> LOWER_TEETH_F;
+
+    
+    public List<GameObject> currentList;
+
     [System.Serializable]
     public class Sequence
     {
         public bool nextAngle;
         public string jawAngle;
+        public bool plagueBrush;
         public bool isEnd;
 
         [TextArea(3, 15)]
@@ -32,6 +49,7 @@ public class Monologue : MonoBehaviour
     [SerializeField] private MonologueFocus monologueFocus;
 
     public bool isTalking = false;
+    public bool isBrushing = false;
     private bool isTyping;
     public int currElement;
     private string currentSentence;
@@ -47,7 +65,9 @@ public class Monologue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isBrushing) {
+            CheckBrushingCompletion();
+        }
     }
 
     public void StartMonologue()
@@ -69,6 +89,10 @@ public class Monologue : MonoBehaviour
         if (Sequences[sequenceNumber].nextAngle) 
         {
             jawSceneAnimation.ChangeAnimationState(Sequences[sequenceNumber].jawAngle);
+            if (Sequences[sequenceNumber].plagueBrush) 
+            {
+                ActivatePlague(Sequences[sequenceNumber].jawAngle);
+            }
         }
         sequenceNumber++;
     }
@@ -119,5 +143,86 @@ public class Monologue : MonoBehaviour
         StopAllCoroutines();
         monologueBox.text = currentSentence;
         isTyping = false;
+    }
+
+    public void ActivatePlague(string angle)
+    {
+        if (angle == "upper_teeth_a") 
+        {
+            ActivateBoxCollider(UPPER_TEETH_A);
+        } 
+        else if (angle == "upper_teeth_b")
+        {
+            ActivateBoxCollider(UPPER_TEETH_B);
+        } 
+        else if (angle == "upper_teeth_c")
+        {
+            ActivateBoxCollider(UPPER_TEETH_C);
+        } 
+        else if (angle == "upper_teeth_d")
+        {
+            ActivateBoxCollider(UPPER_TEETH_D);
+        } 
+        else if (angle == "upper_teeth_e")
+        {
+            ActivateBoxCollider(UPPER_TEETH_E);
+        } 
+        else if (angle == "upper_teeth_f")
+        {
+            ActivateBoxCollider(UPPER_TEETH_F);
+        }
+        else if (angle == "lower_teeth_a")
+        {
+            ActivateBoxCollider(LOWER_TEETH_A);
+        }
+        else if (angle == "lower_teeth_b")
+        {
+            ActivateBoxCollider(LOWER_TEETH_B);
+        }
+        else if (angle == "lower_teeth_c")
+        {
+            ActivateBoxCollider(LOWER_TEETH_C);
+        }
+        else if (angle == "lower_teeth_d")
+        {
+            ActivateBoxCollider(LOWER_TEETH_D);
+        }
+        else if (angle == "lower_teeth_e")
+        {
+            ActivateBoxCollider(LOWER_TEETH_E);
+        }
+        else if (angle == "lower_teeth_f")
+        {
+            ActivateBoxCollider(LOWER_TEETH_F);
+        }
+    }
+
+    public void ActivateBoxCollider(List<GameObject> plagueList)
+    {
+        isBrushing = true;
+        currentList = plagueList;
+        foreach(GameObject plague in plagueList)
+        {
+            plague.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+
+    public void CheckBrushingCompletion()
+    {
+        if (currentList != null) {
+            foreach(GameObject plague in currentList)
+            {
+                if (plague.activeSelf) 
+                {
+                    return;
+                }
+            }
+        }
+        if (!isBrushing) 
+        {
+            return;
+        }
+        isBrushing = false;
+        Invoke("StartMonologue", 2);
     }
 }
